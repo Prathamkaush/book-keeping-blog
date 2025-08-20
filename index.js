86c40b4aa260d0d5ed11794916620a2f76c38559
Prathamkaush <prathamkaushik2002@gmail.com>
2025-08-20 14:19:08 +0530

    Prepare for Render deployment

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ db.connect()
 app.use(
   session({
     store: new pgSession({ pool: db, tableName: "session" }),
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "superSecret",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
